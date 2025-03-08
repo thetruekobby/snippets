8efcbf2a07bfae31dc0537cefa17d3af5472db9d
Pius Agyekum <agyekumpius1@gmail.com>
2025-03-08 16:50:09 +0000

    testing error state

--- src/pages/DisplayUsers/DisplayUsers.jsx
+++ src/pages/DisplayUsers/DisplayUsers.jsx
@@ -2,23 +2,35 @@ import React, { useEffect } from "react"
 import axios from "axios"
 import { useState } from "react"
 
-
 const DisplayUsers = ({ func }) => {
   const [loading, setLoading] = useState(false)
   const [users, setUsers] = useState([])
+  const [error, setError] = useState(null)
 
   const fetchUsers = async () => {
-    setLoading(true)
-    const res = await axios.get("https://jsonplaceholder.typicode.com/users")
-    setUsers(res.data.map((user) => user.name))
-    setLoading(false)
+    try {
+      setLoading(true)
+      const res = await axios.get("https://jsonplaceholder.typicode.com/users")
+      setUsers(res.data.map((user) => user.name))
+    } catch (error) {
+      setError(error.message)
+    } finally {
+      setLoading(false)
+    }
+  }
+  if (error) {
+    return <div className="" data-testid="error">{error}</div>
   }
 
   return (
     <div data-testid="display-users">
       <h1 className="text-center font-bold">Display Users</h1>
-      <button className="p-2 bg-gray-100 border rounded" onClick={fetchUsers}>{loading ? "Loading..." : "Fetch Users"}</button>
-      <button className="p-2 bg-gray-100 border rounded" onClick={func}>run props function</button>
+      <button className="p-2 bg-gray-100 border rounded" onClick={fetchUsers}>
+        {loading ? "Loading..." : "Fetch Users"}
+      </button>
+      <button className="p-2 bg-gray-100 border rounded" onClick={func}>
+        run props function
+      </button>
       <ul>
         {users.map((user) => (
           <li key={user}>{user}</li>
--- src/pages/DisplayUsers/DisplayUsers.test.jsx
+++ src/pages/DisplayUsers/DisplayUsers.test.jsx
@@ -1,10 +1,11 @@
 import React from "react"
-import { findAllByRole, render, screen } from "@testing-library/react"
+import { findAllByRole, findByTestId, render, screen } from "@testing-library/react"
 import { describe, expect, test, vi } from "vitest"
 import DisplayUsers from "./DisplayUsers"
 import "@testing-library/jest-dom/vitest"
 import userEvent from "@testing-library/user-event"
 import { server } from "../../mocks/server"
+import { http, HttpResponse } from "msw"
 
 // server.listen()
 
@@ -18,7 +19,6 @@ describe("Users", () => {
     render(<DisplayUsers func={func} />)
     expect(screen.getByRole("button", { name: /fetch/i })).toBeInTheDocument()
   })
-  
 
   test("mock funtion", async () => {
     const func = vi.fn()
@@ -28,14 +28,28 @@ describe("Users", () => {
     expect(func).toHaveBeenCalledTimes(2)
   })
 
-    test("fetches users on click", async () => {
-      render(<DisplayUsers />)
-      const button = screen.getByRole("button", { name: /fetch/i })
-      await userEvent.click(button)
-      // const loading = await screen.findByText(/loading/i)
-      // expect(loading).toBeInTheDocument()
-      const users = await screen.findAllByRole("listitem")
-      expect(users).toHaveLength(3)
-    })
-
+  test("fetches users on click", async () => {
+    render(<DisplayUsers />)
+    const button = screen.getByRole("button", { name: /fetch/i })
+    await userEvent.click(button)
+    // const loading = await screen.findByText(/loading/i)
+    // expect(loading).toBeInTheDocument()
+    const users = await screen.findAllByRole("listitem")
+    expect(users).toHaveLength(3)
+  })
+  
+  test("renders error", async () => {
+    server.use(
+      http.get("https://jsonplaceholder.typicode.com/users", () => {
+        return HttpResponse.json(null, { status: 500 })
+      })
+    )
+    render(<DisplayUsers />)
+    const button = screen.getByRole("button", { name: /fetch/i })
+    await userEvent.click(button)
+    // const loading = await screen.findByText(/loading/i)
+    // expect(loading).toBeInTheDocument()
+    const error = await screen.findByTestId("error")
+    expect(error).toBeInTheDocument()
+  })
 })
